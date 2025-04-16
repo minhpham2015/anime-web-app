ff3c52a6493134b39b6406e52aff84dd7efa0b72
Pham Thanh Minh <thanhminh1602@gmail.com>
2025-04-16 22:31:40 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+} 
